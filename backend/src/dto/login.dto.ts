80339a9964bc013c44c75f9b02fada3e51b6228f
@agungwramdev <bzee71@gmail.com>
2026-01-15 11:16:49 +0700

    change credential in /api/auth/login di docs

--- backend/src/dto/login.dto.ts
+++ backend/src/dto/login.dto.ts
@@ -2,12 +2,12 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ example: 'superadmin', description: 'Username for login' })
+  @ApiProperty({ example: 'username', description: 'Username for login' })
   @IsString()
   @IsNotEmpty()
   username: string;
 
-  @ApiProperty({ example: 'admin123', description: 'Password for login' })
+  @ApiProperty({ example: 'password', description: 'Password for login' })
   @IsString()
   @IsNotEmpty()
   password: string;
